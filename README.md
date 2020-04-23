cd2140a155f3539f80a6044b501e0db73f90c8d7
MattT <trojanface@hotmail.com>
2020-04-23 16:45:34 +0930

    Added comments and a Readme file for the current project.

--- README.md
+++ README.md
@@ -1,19 +1,38 @@
 
-# example
-### By matt
+# Readme Generator
+### By trojanface
 
-![A screenshot of example](png1.png)
+## Description
+A node script to assist with the creation of comprehensive readme files. By asking the user a series of questions it uses these answers tog enerate the contents of a readme.md file.
 
-![A screenshot of example](png2.png)
+## Table of Contents
 
-![A screenshot of example](png3.png)
+Description
+Contents
+Screenshot
+Deployed
+Installation
+Usage
+License
+Contact
+Address
 
-![A screenshot of example](png4.png)
+![A screenshot of Readme Generator](screenshot1.png)
+
+## Link to Deployed App
+https://not yet deployed
+
+## Installation
+1. Must run a npm install to download inquirer package from npm.
+2. You can then run the program with node.
 
 ## Usage
-this is how you use it
-line 1
-line 2
+1. Run script with node. e.g. (node index.js)
+2. Answer the questions.
+3. Find the created README file in the selected directory.
+
+## License
+Personal Use
 
 ## Contact
-email
+email - trojanface@hotmail.com
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+//Imports the required NPM packages for accepting user input and altering the file system
 const inquirer = require("inquirer");
 const fs = require('fs');
 
+
+//Prompt user with various questions for readme file generation.
 inquirer.prompt([
     {
         type: "input",
         name: "Username",
         message: "gitHub username:",
+        //is validated as the user MUST supply their username.
         validate: (input) => {
             if (input === "") {
                 return "Must be answered";
@@ -17,6 +21,7 @@ inquirer.prompt([
         type: "input",
         name: "Project_Title",
         message: "Project Title:",
+        //is validated as the user MUST supply a project name.
         validate: (input) => {
             if (input === "") {
                 return "Must be answered";
@@ -40,6 +45,11 @@ inquirer.prompt([
         message: "Screenshot Link/s (seperate by , ):"
     },
     {
+        name: "Deployed",
+        type: "input",
+        message: "Link to deployed App https://:"
+    },
+    {
         name: "Installation",
         type: "editor",
         message: "Installation Instructions:"
@@ -50,9 +60,15 @@ inquirer.prompt([
         message: "Usage Instructions:"
     },
     {
+        type: "list",
+        message: "License:",
         name: "License",
-        type: "input",
-        message: "License:"
+        choices: [
+            "Public Domain",
+            "Permissive",
+            "Copyleft",
+            "Proprietary"
+        ]
     },
     {
         name: "Contributors",
@@ -67,11 +83,10 @@ inquirer.prompt([
     {
         type: "list",
         message: "Preferred Contact Method:",
-        name: "Contact",
+        name: "Contact_Preference",
         choices: [
-            "email",
-            "phone",
-            "telekinesis"
+            "Email",
+            "Phone"
         ]
     },
     {
@@ -80,42 +95,56 @@ inquirer.prompt([
         message: "Contact email address:"
     }
 ]).then(function (data) {
+    //creates a prefilled Table of Contents variable and a blank readme string.
     let tableOfContents = "\n## Table of Contents\n";
     let readMeContent = "";
+    //loops through the properties in the user prompts and checks if they're empty. Any empty sections are not included in the readme file.
     for (property in data) {
         if (data[property] !== "") {
+            //Checks for various cases which require individual formatting.
             switch (property) {
+                //table of contents will generate based on the properties which have been answered not including formatting properties such as username and project title.
                 case "Contents":
-                    if (data.Contents === true) {
+                    if (data.Contents === true) { //checks that a contents table has been selected to include.
                         for (property in data) {
-                            if (data[property] != "") {
-                                tableOfContents += `\n${property}`;
+                            if (property != "Username" && property != "Project_Title") { //Ensures current property isn't a formatting property.
+                                if (data[property] != "") {
+                                    tableOfContents += `\n${property}`; //Adds the property to the table of contents variable.
+                                }
                             }
-                            console.log(`key= ${property} value = ${data[property]}`)
                         }
                     } else {
                         tableOfContents = "";
                     }
-                    readMeContent += tableOfContents;
+                    readMeContent += tableOfContents; //Adds the contents table to the readme file.
                     break;
-                case "Project_Title":
+                case "Project_Title": //Uses the formatting properties to create the header and sub-header.
                     readMeContent += `\n# ${data[property]}\n### By ${data.Username}\n`;
                     break;
-                case "Username":
+                case "Username"://Does not include the username property in the default behaviour.
                     break;
-                case "Screenshot":
+                case "Deployed"://Special formatting requirements for the link to the deployed app.
+                    readMeContent += `\n## Link to Deployed App\nhttps://${data.Deployed}\n`;
+                    break;
+                case "Screenshot"://Special formatting requirements for the screenshots.
                     let screenShots = data[property].split(",");
                     screenShots.forEach(element => {
                         readMeContent += `\n![A screenshot of ${data.Project_Title}](${element})\n`;
                     });
                     break;
-                default:
+                    case "Contact_Preference"://Special formatting requirements for the Contact preference
+                        readMeContent += `\n## ${property}\n${data[property]} - `;
+                    break;
+                    case "Address"://Special formatting for the address property
+                        readMeContent += `${data[property]}`;
+                    break;
+                default://By default it adds the property as a title and its value as the text beneath
                     readMeContent += `\n## ${property}\n${data[property]}\n`;
             }
         }
 
     }
-    fs.writeFile("README.md",
+    fs.writeFile("new README.md", //creates a new readme file.
         readMeContent,
         function (err) {
 
